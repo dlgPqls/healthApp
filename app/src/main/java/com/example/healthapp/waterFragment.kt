ed7b0b9ba7b26364eb995a2732f7f3571f43d9c9
김솔 <dndjdfjf10@gmail.com>
2022-01-28 01:59:32 +0900

    코틀린 코드 작성중

--- app/src/main/java/com/example/healthapp/waterFragment.kt
+++ app/src/main/java/com/example/healthapp/waterFragment.kt
@@ -1,8 +1,17 @@
 package com.example.healthapp
 
-
-import android.graphics.Color
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -22,7 +31,46 @@ class waterFragment : Fragment() {
     private val TAG = this.javaClass.simpleName
     lateinit var barChart: BarChart
     private val barData = ArrayList<BarData>()
+    private var per = 10;
+    private var cup = 2;
+    lateinit var mainActivity: MainActivity
 
+    lateinit var waterPercent : TextView
+    lateinit var waterText : TextView
+    lateinit var editAmount: EditText
+    lateinit var waterButton: Button
+
+    lateinit var myHelper : workFragment.myDBHelper
+    lateinit var sqlDB: SQLiteDatabase
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        waterPercent = view.findViewById(R.id.waterPercent)
+        waterText = view.findViewById(R.id.waterText)
+        editAmount = view.findViewById(R.id.editAmount)
+        waterButton = view.findViewById(R.id.waterButton)
+
+        //val intent = intent
+        //id = intent.getStringExtra("intent_name").toString()
+        myHelper = workFragment.myDBHelper(mainActivity)
+
+        waterButton.setOnClickListener{
+            sqlDB = myHelper.writableDatabase
+            sqlDB.execSQL("UPDATE mainTBL SET mWater ='"+ editAmount.text.toString().toInt() + "' WHERE mName = + 'id';"  )
+            sqlDB.close()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mainActivity.findViewById<TextView>(R.id.waterPercent).text = "$per"
+        mainActivity.findViewById<TextView>(R.id.waterText).text = "오늘 하루 " + "$cup" +"잔의 물을 마셨어요"
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/healthapp/workFragment.kt
+++ app/src/main/java/com/example/healthapp/workFragment.kt
@@ -1,10 +1,22 @@
 package com.example.healthapp
 
+import android.content.Context
+import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.LinearLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
+import kotlin.concurrent.timer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,15 +26,67 @@ import android.view.ViewGroup
  * create an instance of this fragment.
  */
 class workFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var per = 1;
+    private var workTime : Int = 0
+    private var many : Int = 10
+
+    lateinit var workPercent : TextView
+    lateinit var workText : TextView
+    lateinit var editHour: EditText
+    lateinit var editMinute: EditText
+    lateinit var workButton: Button
+    lateinit var mainActivity: MainActivity
+
+    lateinit var myHelper : myDBHelper
+    lateinit var sqlDB: SQLiteDatabase
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        workPercent = view.findViewById(R.id.workPercent)
+        workText = view.findViewById(R.id.workText)
+        editHour = view.findViewById(R.id.editHour)
+        editMinute = view.findViewById(R.id.editMinute)
+        workButton = view.findViewById(R.id.workButton)
+
+        //로그인페이지에서 회원 id 값 intent로 받아와서 sql문으로 넘기면 될 듯. 해당 id의 운동량 목표, 물 섭취량 목표 행에 값 넣기
+
+        myHelper = myDBHelper(mainActivity)
+
+        workButton.setOnClickListener{
+            workTime = (editHour.text.toString().toInt()*60) + (editMinute.text.toString().toInt())
+            sqlDB = myHelper.writableDatabase
+            sqlDB.execSQL("UPDATE mainTBL SET mWork ='"+ workTime + "' WHERE mName = + 'id';"  )
+            sqlDB.close()
+        }
+    }
+
+    class myDBHelper(context: Context) : SQLiteOpenHelper(context, "groupDB", null, 1){
+        override fun onCreate(db: SQLiteDatabase?) {
+            db!!.execSQL("CREATE TABLE mainTBL (mName CHAR(20) PRIMARY KEY, mWork Integer, mWater Integer);")
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+            db!!.execSQL("DROP TABLE IF EXISTS mainTBL")
+            onCreate(db)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mainActivity.findViewById<TextView>(R.id.workPercent).text = "$per"
+        mainActivity.findViewById<TextView>(R.id.workText).text = "오늘 하루 " + "$many" +"분 운동했어요"
     }
 
     override fun onCreateView(

2794bdf42a2e3a19f4b905f8978fd19f0a82b9f7
kimhaebin <rlao99@naver.com>
2022-01-30 19:29:03 +0900

    mypage ui

---
+++ app/src/main/java/com/example/healthapp/WaterActivity.kt
@@ -0,0 +1,21 @@
+package com.example.healthapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_water.*
+
+class WaterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_water)
+
+        btn_a.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java) // 운동페이지로 이동
+            startActivity(intent)
+        }
+    }
+}
+        }
+
